3a9d97ab1ff4a2cc9396cfaefc3430b35d703c2a
explorer-27 <207ashu@gmail.com>
2018-03-21 23:30:14 +0530

    Images Loaded & Adding to cart functionality included

--- app/src/main/java/com/example/ashu/wendor/MainActivity.java
+++ app/src/main/java/com/example/ashu/wendor/MainActivity.java
@@ -1,13 +1,183 @@
 package com.example.ashu.wendor;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ashu.wendor.Images.CustomSwipeAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    static ViewPager viewPager;
+    static ArrayList<Items> list;
+    static ArrayList<String> cartlist;
+    static int currentPos;
+    CustomSwipeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        list = new ArrayList<>();
+        cartlist = new ArrayList<>();
+        getSupportLoaderManager().initLoader(0, null, this);
+
+        viewPager = findViewById(R.id.view_pager);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemSelected = item.getItemId();
+        if (itemSelected == R.id.cartmenu) {
+            Intent i = new Intent(this, CartActivity.class);
+            i.putStringArrayListExtra("cartList", cartlist);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void addToCartClicked(View view) {
+        currentPos = viewPager.getCurrentItem();
+        Items currentItem = list.get(currentPos);
+
+        final int itemId = currentItem.getItemId();
+        final String itemName = currentItem.getName();
+        int price = currentItem.getPrice();
+        final int leftQty = currentItem.getLeftUnit();
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_layout, null);
+        mBuilder.setView(mView);
+
+        final TextView tvNameDialog = (TextView) mView.findViewById(R.id.tvName);
+        final TextView tvPriceDialog = (TextView) mView.findViewById(R.id.tvPrice);
+        final EditText mQty = (EditText) mView.findViewById(R.id.etQty);
+        Button btnYes = (Button) mView.findViewById(R.id.btnYes);
+        Button btnNo = (Button) mView.findViewById(R.id.btnNo);
+
+        tvNameDialog.setText(itemName);
+        tvPriceDialog.setText("Rs. " + price);
+
+        mQty.setHint(leftQty + " quantities left.");
+
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int qtyEntered = 0;
+                try {
+                    qtyEntered = Integer.parseInt(mQty.getText().toString());
+                    if (qtyEntered > 0 && qtyEntered <= leftQty) {
+
+                        cartlist.add(itemName);
+                        dialog.dismiss();
+                    } else {
+                        Toast.makeText(MainActivity.this,
+                                "Please Enter Correct Qty",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    /*Intent i=new Intent(MainActivity.this,CartActivity.class);
+                    i.putStringArrayListExtra("cartList",cartlist);
+                        startActivity(i);*/
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this,
+                            "Please Enter Correct Qty",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        mBuilder.setView(null);
+
+
+        // Toast.makeText(this,""+currentItem.getItemId(),Toast.LENGTH_SHORT).show();
+/*
+      Intent i=new Intent(this,CartActivity.class);
+        i.putExtra("postiton", currentItem.getName());
+        startActivity(i);*/
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, WendorContentProvider.CONTENT_URI, null, null, null, WendorContentProvider.name);
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data.getCount() == 0) {
+            Toast.makeText(this, "Count==0", Toast.LENGTH_SHORT).show();
+        }
+        if (data.moveToFirst()) {
+            do {
+
+                String name = data.getString(data.getColumnIndex(WendorContentProvider.name));
+                String imageUrl = data.getString(data.getColumnIndex(WendorContentProvider.imageUrl));
+                String imagePath = data.getString(data.getColumnIndex(WendorContentProvider.imagePath));
+                int totUnits = data.getInt(data.getColumnIndex(WendorContentProvider.totUnits));
+                int leftUnits = data.getInt(data.getColumnIndex(WendorContentProvider.leftUnits));
+                int price = data.getInt(data.getColumnIndex(WendorContentProvider.price));
+                int itemId = data.getInt(data.getColumnIndex(WendorContentProvider.itemId));
+
+                Items item = new Items();
+                item.setItemId(itemId);
+                item.setImgPath(imagePath);
+                item.setName(name);
+                item.setImgUrl(imageUrl);
+                item.setLeftUnit(leftUnits);
+                item.setTotUnits(totUnits);
+                item.setPrice(price);
+                list.add(item);
+
+                // loadImages(list);
+                //Toast.makeText(this,""+imagePath,Toast.LENGTH_SHORT).show();
+            } while (data.moveToNext());
+
+            adapter = new CustomSwipeAdapter(this, list);
+            viewPager.setAdapter(adapter);
+
+        }
+
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
+
--- app/src/main/java/com/example/ashu/wendor/SplashActivity.java
+++ app/src/main/java/com/example/ashu/wendor/SplashActivity.java
@@ -1,16 +1,31 @@
 package com.example.ashu.wendor;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.URL;
 
 public class SplashActivity extends AppCompatActivity {
+    static Context appContext;
+    boolean InternetStatus;
+    private String imageDir = "imageDir";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        appContext = getApplicationContext();
+        final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+
         new Handler().postDelayed(new Runnable() {
 
             /*
@@ -22,12 +37,73 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
 
-                // close this activity
-                finish();
+                if (sp.getBoolean("firstrun", true)) {
+                    /*
+                        *When App launches for the First time....
+                        *Load json file...
+                        *And Create a Database....
+                    */
+
+                    URL jsonUrl = NetworkUtility.buildUrl();
+                    new QueryTask().execute(jsonUrl);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putBoolean("firstrun", false).commit();
+                } else {
+                    finish();
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    i.putExtra("json", "HEllo");
+                    startActivity(i);
+                }
             }
         }, 3000);
+
+    }
+
+    public boolean checkInternet() {
+        boolean internetStatus = true;
+        ConnectivityManager connec = (ConnectivityManager) getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
+        if (connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED || connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED
+                || connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTING || connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTING)
+            internetStatus = true;
+        else if (connec.getNetworkInfo(0).getState() == NetworkInfo.State.DISCONNECTED || connec.getNetworkInfo(1).getState() == NetworkInfo.State.DISCONNECTED)
+            internetStatus = false;
+
+        return internetStatus;
+    }
+
+    //Used for fetching json
+    private class QueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String searchResults = null;
+            try {
+                InternetStatus = checkInternet();
+                if (InternetStatus)
+                    searchResults = NetworkUtility.getResponseFromHttpUrl(searchUrl);
+                else
+                    searchResults = "No Internet";
+            } catch (Exception e) {
+                e.printStackTrace();
+                searchResults = null;
+            }
+            return searchResults;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.i("Wendor", s);
+            JsonData.getJsonData(s, SplashActivity.this);
+            finish();
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+
+        }
     }
 }
